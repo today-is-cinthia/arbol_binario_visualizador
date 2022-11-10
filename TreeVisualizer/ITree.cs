d4ff63195e09e3e8ec846e585519fe558c25e5ce
Cinthia <87044276+today-is-cinthia@users.noreply.github.com>
2022-11-09 21:08:21 -0600

    Add files via upload

---
+++ TreeVisualizer/ITree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeVisualizer
+{
+    public interface ITree
+    {
+        IEnumerable<NodeInfo> GetAllNodes();
+
+        TreeConfiguration GetConfiguration();
+    }
+
+    public interface ITree<TValue> : ITree where TValue : IComparable<TValue>
+    {
+        void Insert(TValue value);
+
+        void Remove(TValue value);
+    }
+}
